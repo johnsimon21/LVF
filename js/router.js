c2127dee6a0ad584b3f5d21ac956d983fa6aa6f9
johnsimon21 <120222249+johnsimon21@users.noreply.github.com>
2025-04-05 19:51:31 +0100

    replace lorem route with contact route in router configuration

--- js/router.js
+++ js/router.js
@@ -9,7 +9,7 @@ const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
     "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
+    "/contact": "/pages/contact.html",
     
     // Grade 10 routes
     // Kinematics
